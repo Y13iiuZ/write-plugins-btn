0f24b123c69bf69899ed1ea0283cc1b144bba017
ztx <tiexizhang0323@gmail.com>
2024-12-29 15:00:09 +0800

    fix: Add old gitignore

---
+++ vite.config.mjs
@@ -0,0 +1,20 @@
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+const lifecycle = process.env.npm_lifecycle_event // 当前正在运行的 npm 脚本的名称
+console.log(lifecycle, '当前运行的脚本名字');
+
+// https://vitejs.dev/config/
+// /** @type {import('vite').UserConfig} */
+
+export default defineConfig({
+  plugins: [vue()],
+  lintOnSave:false,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // 相对需要base，绝对忽略base
+    }
+  },
+})
